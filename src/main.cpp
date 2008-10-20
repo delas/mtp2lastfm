2df56eb6a7ea836d1720d85aa146b34ada1024ca
Andrea <delas@users.noreply.github.com>
2008-10-20 20:45:17 +0000

    - Added CLI parsing with GNU/getop standard definition - Added command for version information

--- src/main.cpp
+++ src/main.cpp
@@ -26,63 +26,97 @@
 
 #include <iostream>
 #include <vector>
+#include <getopt.h>
+
 #include "track.h"
 #include "device.h"
 #include "scrobbler.h"
 
 #define print(x) cout << x << endl;
-#define verbose(x) if (verbose_mode) print(x)
+#define verbose(x) if (flag_verbose_mode) print(x)
 
 using namespace std;
 
+
 void help(const char* cmd)
 {
-	print("Usage: " << cmd << " [options]")
+	/* header */
+	print("mtp2lastfm " << CLIENT_VERSION)
+	print("")
+	/* help content */
+	print("Basic usages: " << cmd << " [options]")
 	print("Options:")
 	print("  -i, --import        Import all the player track, and mark as already scrobbled")
 	print("  -l, --list-devices  List all connected devices")
-	print("  -v                  Verbose mode")
-	print("  --help              Display this information")
+	print("  -v, --verbose       Verbose mode")
+	print("  -V, --version       Version information")
+	print("  -h, --help          Display this information")
 	print("")
 }
 
+void version()
+{
+	/* header */
+	print("mtp2lastfm " << CLIENT_VERSION)
+	print("")
+	/* version content */
+	print("Copyright (C) 2008 - Andrea Burattin")
+	print("")
+	print("This program comes with NO WARRANTY, to the extent permitted by law.")
+	print("You may redistribute copies of this program under the terms of")
+	print("the GNU General Public License.")
+}
+
+
 int main(int argc, char* argv[])
 {
-	bool only_import = false;
-	bool only_list_device = false;
-	bool verbose_mode = false;
-	string configfile = (string(getenv("HOME")) + "/.mtp2lastfm");
-	device d;
+	bool flag_only_import = false;
+	bool flag_only_list_device = false;
+	bool flag_verbose_mode = false;
 
-	/* header */
-	print("mtp2lastfm v." << CLIENT_VERSION)
-	print("================")
-	print("")
-
-	/* parameters */
-	for (int i = 1; i < argc; i++)
+	/* command line parsing */
+	int c;
+	int option_index = 0; /* getopt_long stores the option index here */
+	static const struct option long_options[] = {
+		{"help",         no_argument, 0, 'h'},
+		{"import",       no_argument, 0, 'i'},
+		{"list-devices", no_argument, 0, 'l'},
+		{"verbose",      no_argument, 0, 'v'},
+		{"version",      no_argument, 0, 'V'},
+		{ 0, 0, 0, 0 }
+	};
+	while ((c = getopt_long(argc, argv, "hilvV",
+	                        long_options, &option_index)) != -1)
 	{
-		if (strcmp(argv[i], "-i")==0 || strcmp(argv[i], "--import")==0)
+		switch (c)
 		{
-			only_import = true;
-		}
-		if (strcmp(argv[i], "-l")==0 || strcmp(argv[i], "--list-devices")==0)
-		{
-			only_list_device = true;
-		}
-		if (strcmp(argv[i], "-v")==0)
-		{
-			verbose_mode = true;
-		}
-		else if (strcmp(argv[i], "--help")==0)
-		{
-			help(argv[0]);
-			return 0;
+			case 'h':
+				help(argv[0]);
+				return 0;
+			case 'i':
+				flag_only_import = true;
+				break;
+			case 'l':
+				flag_only_list_device = true;
+				break;
+			case 'v':
+				flag_verbose_mode = true;
+				break;
+			case 'V':
+				version();
+				return 0;
+			default:
+				return 1;
 		}
 	}
 
+	/* base objects */
+	verbose("Internal objects declaration")
+	string configfile = (string(getenv("HOME")) + "/.mtp2lastfm");
+	device d;
+
 	/* list all connected device */
-	if (only_list_device)
+	if (flag_only_list_device)
 	{
 		verbose("Getting all connected devices");
 		vector<device> vd = device::getAllConnectedDevices();
